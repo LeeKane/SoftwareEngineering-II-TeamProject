66309c4891bbeeec354dc5541ac5a6443d02f90d
NonexistentAkarin <dyf14@software.nju.edu.cn>
2015-10-25 23:24:10 +0800

    WareHouseBLService

--- client/src/main/java/blservice/warehouseblservice/WarehouseBLService.java
+++ client/src/main/java/blservice/warehouseblservice/WarehouseBLService.java
@@ -1,5 +1,21 @@
 package blservice.warehouseblservice;
 
-public class WarehouseBLService {
+import po.list.ListPO;
+import util.ResultMessage;
 
+public interface WarehouseBLService {
+	
+	//飞机装运管理
+	public ResultMessage setPlaneShipping (ListPO list);
+	
+	//火车装运管理
+	public ResultMessage setTrainShipping (ListPO list);
+	
+	//汽车装运管理
+	public ResultMessage setCarShipping (ListPO list);
+	
+	//
+	public ResultMessage geneCenterList();
+	
+	
 }
