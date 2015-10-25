9e6b76e889d03f8a0fd1851d9a2985ef74c7425a
NonexistentAkarin <dyf14@software.nju.edu.cn>
2015-10-25 19:05:51 +0800

    povo

--- client/src/main/java/blservice/financeblservice/FinanceBLService.java
+++ client/src/main/java/blservice/financeblservice/FinanceBLService.java
@@ -1,5 +1,17 @@
 package blservice.financeblservice;
 
-public class FinanceBLService {
+import po.*;
+import util.*;
 
+public class FinanceBLService {
+	//结算管理
+	public ResultMessage BalanceMan (FinancePO po) {
+		return null;
+	}
+	
+	//
+	public ResultMessage CostMan (BAccount ba，costchartPO po){
+		return null;
+	}
+		
 }
---
+++ client/src/main/java/po/list/ListPO.java
@@ -0,0 +1,5 @@
+package po.list;
+
+public class ListPO {
+
+}
