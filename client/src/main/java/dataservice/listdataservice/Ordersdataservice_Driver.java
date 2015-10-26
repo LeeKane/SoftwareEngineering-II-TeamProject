c72dd270bba5ae99e9f8fba6f93ea3290b889d93
JohnsonFu <flh14@software.nju.edu.cn>
2015-10-26 14:32:13 +0800

    Ordersdata驱动和桩

---
+++ client/src/main/java/dataservice/listdataservice/Ordersdataservice_Driver.java
@@ -0,0 +1,21 @@
+package dataservice.listdataservice;
+import java.rmi.RemoteException;
+
+import dataservice.warehousedataservice.WarehouseDataService;
+import dataservice.warehousedataservice.Warehousedataservice_Driver;
+import dataservice.warehousedataservice.Warehousedataservice_Stub;
+import po.*;
+import util.DeliverType;
+public class Ordersdataservice_Driver {
+ public void drive(OrdersDataService service){
+	 TimePO time=new TimePO(1,1,1,1,1,1);
+	 WarePO po=new WarePO (12345 , 10, 10, "1", "book",DeliverType.ECO,156, time);
+	 service.insert(po);
+ }
+ public static void main(String[] args) throws RemoteException{
+		OrdersDataService service=new Ordersdataservice_Stub();
+		Ordersdataservice_Driver driver=new Ordersdataservice_Driver();
+		driver.drive(service);
+	 
+ }
+}
---
+++ client/src/main/java/dataservice/listdataservice/Ordersdataservice_Stub.java
@@ -0,0 +1,15 @@
+package dataservice.listdataservice;
+
+import po.WarePO;
+
+public class Ordersdataservice_Stub implements OrdersDataService {
+
+	@Override
+	
+	public boolean insert(WarePO ware) {
+		// TODO Auto-generated method stub
+		System.out.println("INSERT FAILURE");
+		return false;
+	}
+
+}
