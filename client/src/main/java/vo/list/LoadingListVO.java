ded30092eb61f84534b1f54f9fae244de9bad4ec
JohnsonFu <flh14@software.nju.edu.cn>
2015-11-28 21:47:27 +0800

    list类的VO都谢了

---
+++ client/src/main/java/vo/list/LoadingListVO.java
@@ -0,0 +1,81 @@
+package vo.list;
+
+import java.util.Vector;
+
+import po.TimePO;
+import util.City;
+import util.ListType;
+
+public class LoadingListVO extends Vector<String> {
+	private long id;
+	private ListType type=ListType.TRANS;
+	private TimePO loadDate;//装车日期
+	private long transNum;//本营业厅汽运编号
+	private City departPlace;//出发地
+	private City destination;//到达地
+	private long[] waybillNumList;//本次装箱所有托运单号
+	private String loadMonitor;//监装员
+	private String loadPerformer;//押运员
+	private double freight;//运费
+	public LoadingListVO(long id, ListType type, TimePO loadDate,
+			long transNum, City departPlace, City destination,
+			long[] waybillNumList, String loadMonitor, String loadPerformer,
+			double freight) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.loadDate = loadDate;
+		this.transNum = transNum;
+		this.departPlace = departPlace;
+		this.destination = destination;
+		this.waybillNumList = waybillNumList;
+		this.loadMonitor = loadMonitor;
+		this.loadPerformer = loadPerformer;
+		this.freight = freight;
+		this.add(id+"");
+		this.add(type.toString());
+		this.add(loadDate.toString());
+		this.add(transNum+"");
+		this.add(departPlace.toString());
+		this.add(destination.toString());
+		for(int i=0;i<waybillNumList.length;i++){
+			this.add(waybillNumList[i]+"");
+			
+		}
+		this.add(loadMonitor);
+		this.add(loadPerformer);
+		this.add(freight+"");
+	}
+	public long getId() {
+		return id;
+	}
+	public ListType getType() {
+		return type;
+	}
+	public TimePO getLoadDate() {
+		return loadDate;
+	}
+	public long getTransNum() {
+		return transNum;
+	}
+	public City getDepartPlace() {
+		return departPlace;
+	}
+	public City getDestination() {
+		return destination;
+	}
+	public long[] getWaybillNumList() {
+		return waybillNumList;
+	}
+	public String getLoadMonitor() {
+		return loadMonitor;
+	}
+	public String getLoadPerformer() {
+		return loadPerformer;
+	}
+	public double getFreight() {
+		return freight;
+	}
+	
+	
+	}
--- client/src/main/java/vo/list/TransCenterArrivalListVO.java
+++ client/src/main/java/vo/list/TransCenterArrivalListVO.java
@@ -1,22 +1,18 @@
-package po.list;
+package vo.list;
 
-import java.io.Serializable;
+import java.util.Vector;
 
 import po.TimePO;
 import util.City;
 import util.GoodState;
 
-public class TranscenterArrivalList implements Serializable {
-
+public class TransCenterArrivalListVO extends Vector<String> {
 	private long transcenterID;/*中转中心编号*/
 	private TimePO arrivatime;
 	private long translistID;/*中转单编号*/
 	private City startCity;
 	private GoodState state;/*货物到达状态*/
-	
-	
-	
-	public TranscenterArrivalList(long transcenterID, TimePO arrivatime,
+	public TransCenterArrivalListVO(long transcenterID, TimePO arrivatime,
 			long translistID, City startCity, GoodState state) {
 		super();
 		this.transcenterID = transcenterID;
@@ -24,39 +20,29 @@ public class TranscenterArrivalList implements Serializable {
 		this.translistID = translistID;
 		this.startCity = startCity;
 		this.state = state;
+		this.add(transcenterID+"");
+		this.add(arrivatime.toString());
+		this.add(translistID+"");
+		this.add(startCity.toString());
+		this.add(state.toString());
 	}
 	public long getTranscenterID() {
 		return transcenterID;
 	}
-	public void setTranscenterID(long transcenterID) {
-		this.transcenterID = transcenterID;
-	}
 	public TimePO getArrivatime() {
 		return arrivatime;
-	}
-	public void setArrivatime(TimePO arrivatime) {
-		this.arrivatime = arrivatime;
 	}
 	public long getTranslistID() {
 		return translistID;
 	}
-	public void setTranslistID(long translistID) {
-		this.translistID = translistID;
-	}
 	public City getStartCity() {
 		return startCity;
-	}
-	public void setStartCity(City startCity) {
-		this.startCity = startCity;
 	}
 	public GoodState getState() {
 		return state;
 	}
-	public void setState(GoodState state) {
-		this.state = state;
-	}
 	
 	
 	
-	
+
 }
