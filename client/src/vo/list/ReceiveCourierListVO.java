5fbbd10d4d1e6ac37309808d5d2efaa6f66d3d7b
NonexistentAkarin <dyf14@software.nju.edu.cn>
2015-12-02 21:05:49 +0800

    asdfasda

--- client/src/vo/list/ReceiveCourierListVO.java
+++ client/src/vo/list/ReceiveCourierListVO.java
@@ -3,23 +3,27 @@ package vo.list;
 import java.util.Vector;
 
 import po.TimePO;
+import util.ListState;
 
 public class ReceiveCourierListVO extends Vector<String>{
 	private TimePO time;//收件日期（时间）
 	private long id;//收件编号
 	private String name;//收件人姓名
 	private String cellphoneNum;//收件人手机
+	private ListState state;//审批状态
 	
-	public ReceiveCourierListVO(TimePO time, long id, String name, String cellphoneNum) {
+	public ReceiveCourierListVO(TimePO time, long id, String name, String cellphoneNum, ListState state) {
 		this.time = time;
 		this.id = id;
 		this.name = name;
 		this.cellphoneNum = cellphoneNum;
+		this.state=state;
 	
 		this.add(time.toString());
 		this.add(id+"");
 		this.add(name);
 		this.add(cellphoneNum);
+		this.add(state.toString());
 	}
 
 	public TimePO getTime() {
@@ -36,5 +40,9 @@ public class ReceiveCourierListVO extends Vector<String>{
 
 	public String getCellphoneNum() {
 		return cellphoneNum;
+	}
+
+	public ListState getState() {
+		return state;
 	}
 }
