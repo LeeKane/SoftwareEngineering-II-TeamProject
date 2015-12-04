ed3c021b3976af73c5f00e5249d58806d9d137ea
JohnsonFu <flh14@software.nju.edu.cn>
2015-12-04 18:58:31 +0800

    MoneyIn

---
+++ client/src/dataservice/MoneyInListDataService/MoneyInListDataService.java
@@ -0,0 +1,12 @@
+package dataservice.MoneyInListDataService;
+
+import po.list.MoneyInListPO;
+
+public interface MoneyInListDataService {
+	
+	public void init();
+	public void insert(MoneyInListPO po);
+	public MoneyInListPO find(long id);
+	
+
+}
