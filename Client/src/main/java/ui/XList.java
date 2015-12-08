46b5bf7d4ff3353db1eca8f20abae0c04618d335
NonexistentAkarin <dyf14@software.nju.edu.cn>
2015-12-08 19:42:17 +0800

    再来一波

---
+++ Client/src/main/java/ui/XList.java
@@ -0,0 +1,21 @@
+package ui;
+
+import javax.swing.JList;
+
+public class XList extends JList
+{
+
+	public XList()
+	{
+		init();
+	}
+
+	private void init()
+	{
+		setFont(XContorlUtil.FONT_12_BOLD);
+		setForeground(XContorlUtil.DEFAULT_TEXT_COLOR);
+		setBackground(XContorlUtil.LIST_BACKGROUND);
+		setCellRenderer(new XListRenderer(this));
+		setSelectionMode(0);
+	}
+}
---
+++ Client/src/main/java/ui/chart/ChartPanelTest2.java
@@ -0,0 +1,28 @@
+package ui.chart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+public class ChartPanelTest2
+{
+	public JPanel getChartPanel()
+	{
+		List<JPanel> chartPanels = new ArrayList<JPanel>();
+		//柱图
+		chartPanels.add(new StackBarChart());
+		chartPanels.add(new StackBarChart2());
+//		chartPanels.add(new AreaChart());//this
+		//线图
+		chartPanels.add(new PieChart());
+		chartPanels.add(new GanttChart());
+		chartPanels.add(new DialChart());
+		chartPanels.add(new MeterChart());
+		chartPanels.add(new BubbleChart());
+		chartPanels.add(new ScatterChart());
+		
+		XChartPanel chartPanel = new XChartPanel(3,3,chartPanels);
+		return chartPanel;
+	}
+}
